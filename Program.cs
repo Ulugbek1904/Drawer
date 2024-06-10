58d613ba63e6002d087278ab9837cd9a360fc82d
Ulugbek1904 <166475914+Ulugbek1904@users.noreply.github.com>
2024-06-10 21:17:10 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Diagnostics;
+using System;
 
 namespace Drawer
 {
@@ -8,173 +6,219 @@ namespace Drawer
     {
         public static void Main(string[] args)
         {
-            System.Console.WriteLine("\t\t\t Welcome to Drawer");
+            Console.WriteLine("\t\t\tWelcome to Drawer");
 
             bool continueDrawing = true;
 
-            // while orqali biz dasturni qachon tugashini optional qila olamiz
-
-            while(continueDrawing)
+            while (continueDrawing)
             {
-                System.Console.WriteLine("\n\n");
-                System.Console.WriteLine("Select wanted shape from menu :");
-
-                System.Console.WriteLine("1. Square ".PadRight(30) + "4. Line");
-                System.Console.WriteLine("2. Triangle".PadRight(30) + "5. rectangle");
-                System.Console.WriteLine("3. Circle".PadRight(30) + "6. rhombus");
-
-                string Userinput = Console.ReadLine();
-                int input = int.Parse(Userinput);
-
-                // shakllarni user istagan belgi orqali chiza oladi
-                System.Console.WriteLine("What symbol do you want to draw ? e.g *, ^");
-                string userInput = Console.ReadLine();
-                char incomingChar = char.Parse(userInput);
-
-                switch (input)
+                try
                 {
-                    case 1:
-                        drawSquare(incomingChar);
-                        break;
-                    case 2:
-                        drawTriangle(incomingChar);
-                        break;
-                    case 3: 
-                        drawCircle(incomingChar);
-                        break;
-                    case 4:
-                        drawSLine(incomingChar);
-                        break;
-                    case 5: 
-                        drawRectangle(incomingChar);
-                        break;
-                    case 6:
-                        drawPrhombus(incomingChar);
-                        break;
-                    default :
-                        Console.WriteLine("Please enter a number range 1 to 6");
-                        break;
+                    Console.WriteLine("\n\nSelect wanted shape from menu:");
+                    Console.WriteLine("1. Square".PadRight(30) + "4. Line");
+                    Console.WriteLine("2. Triangle".PadRight(30) + "5. Rectangle");
+                    Console.WriteLine("3. Circle".PadRight(30) + "6. Rhombus");
+                    Console.WriteLine();
+
+                    string userInput = Console.ReadLine();
+                    if (!int.TryParse(userInput, out int input) || input < 1 || input > 6)
+                    {
+                        throw new Exception("Your input must be an integer in the range from 1 to 6.");
+                    }
+
+                    Console.WriteLine("What symbol do you want to draw? e.g *, ^");
+                    string symbolInput = Console.ReadLine();
+                    if (string.IsNullOrEmpty(symbolInput) || symbolInput.Length != 1)
+                    {
+                        throw new Exception("Please enter a single character.");
+                    }
+                    char incomingChar = symbolInput[0];
+
+                    switch (input)
+                    {
+                        case 1:
+                            drawSquare(incomingChar);
+                            break;
+                        case 2:
+                            drawTriangle(incomingChar);
+                            break;
+                        case 3:
+                            drawCircle(incomingChar);
+                            break;
+                        case 4:
+                            drawSLine(incomingChar);
+                            break;
+                        case 5:
+                            drawRectangle(incomingChar);
+                            break;
+                        case 6:
+                            drawPrhombus(incomingChar);
+                            break;
+                        default:
+                            Console.WriteLine("Please enter a number in the range 1 to 6.");
+                            break;
+                    }
+
+                    Console.WriteLine("Do you want to draw again? yes/no");
+                    string yesno = Console.ReadLine().ToLower();
+                    continueDrawing = yesno == "yes";
                 }
-                
-                Thread.Sleep(1000);
-
-                System.Console.WriteLine("Do you want to do again? yes/no");
-                string yesno = Console.ReadLine();
-                continueDrawing = yesno.ToLower() == "yes" ;
-
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input format. Please enter valid values.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
             }
-
         }
 
-        // draw methodlarini alohida qilib olish dasturni ishlashini yaxshilaydi.
         public static void drawSquare(char charac)
         {
-            System.Console.Write("enter side :");
-            int inp = int.Parse(Console.ReadLine());
-            for (int heigth = 0; heigth < inp; heigth++)
+            try
             {
-                for (int width =0; width < inp; width++)
+                Console.Write("Enter side length: ");
+                int side = int.Parse(Console.ReadLine());
+                for (int height = 0; height < side; height++)
                 {
-                    System.Console.Write(charac + "  ");
+                    for (int width = 0; width < side; width++)
+                    {
+                        Console.Write(charac + "  ");
+                    }
+                    Console.WriteLine();
                 }
-                System.Console.WriteLine();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input format. Please enter a numeric value.");
             }
         }
+
         public static void drawTriangle(char charac)
         {
-            Console.Write("Enter height : ");
-            int num = int.Parse(Console.ReadLine());
-
-            for (int i = num; i >= 1; i--)
+            try
             {
-                int count = 0;
-                for (int space = 1; space <= i; space++)
-                {
-                    Console.Write(" ");
-                    count++;
-                }
+                Console.Write("Enter height: ");
+                int num = int.Parse(Console.ReadLine());
 
-                for (int j = 1; j <= num - count; j++)
+                for (int i = num; i >= 1; i--)
                 {
-                    Console.Write("*");
-                }
+                    int count = 0;
+                    for (int space = 1; space <= i; space++)
+                    {
+                        Console.Write(" ");
+                        count++;
+                    }
 
-                for (int j1 = 0; j1 <= num - i; j1++)
-                {
-                    Console.Write("*");
-                }
+                    for (int j = 1; j <= num - count; j++)
+                    {
+                        Console.Write(charac);
+                    }
 
-                Console.WriteLine();
+                    for (int j1 = 0; j1 <= num - i; j1++)
+                    {
+                        Console.Write(charac);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input format. Please enter a numeric value.");
             }
         }
+
         public static void drawRectangle(char charac)
         {
-            System.Console.Write("enter Heigth :");
-            int inp = int.Parse(Console.ReadLine());
-            System.Console.Write("enter width :");
-            int inp2 = int.Parse(Console.ReadLine());
-
-            for (int heigth = 0; heigth < inp; heigth++)
+            try
             {
-                for (int width =0; width < inp2; width++)
+                Console.Write("Enter height: ");
+                int height = int.Parse(Console.ReadLine());
+                Console.Write("Enter width: ");
+                int width = int.Parse(Console.ReadLine());
+
+                for (int h = 0; h < height; h++)
                 {
-                    System.Console.Write(charac + "  ");
+                    for (int w = 0; w < width; w++)
+                    {
+                        Console.Write(charac + "  ");
+                    }
+                    Console.WriteLine();
                 }
-                System.Console.WriteLine();
             }
-            
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input format. Please enter numeric values.");
+            }
         }
+
         public static void drawCircle(char charac)
         {
-            System.Console.WriteLine("circle");
-            
+            Console.WriteLine("Circle drawing is not implemented yet.");
         }
+
         public static void drawPrhombus(char charac)
         {
-            Console.Write("enter halfHeigth : ");
-            int num = int.Parse(Console.ReadLine());
-
-            for (int i =num; i >= 1; i--)
+            try
             {
-                int count = 0;
-                for (int space = 1; space < i; space++)
-                {
-                    Console.Write(" ");
-                    count++;
-                }
-                for (int j =1; j <= num - count; j++)
-                {
-                    Console.Write("* ");
-                }
-                Console.WriteLine();
-            }
-            for (int i = 1; i <= num; i++)
-            {
-                int count = 0;
-                for (int space = 1; space < i; space++)
-                {
-                    Console.Write(" ");
-                    count++;
-                }
-                for(int j = num - count; j >= 1; j--)
-                {
-                    Console.Write("* ");
-                }
+                Console.Write("Enter half height: ");
+                int num = int.Parse(Console.ReadLine());
 
-                Console.WriteLine();
+                for (int i = num; i >= 1; i--)
+                {
+                    int count = 0;
+                    for (int space = 1; space < i; space++)
+                    {
+                        Console.Write(" ");
+                        count++;
+                    }
+                    for (int j = 1; j <= num - count; j++)
+                    {
+                        Console.Write($"{charac} ");
+                    }
+                    Console.WriteLine();
+                }
+                for (int i = 1; i <= num; i++)
+                {
+                    int count = 0;
+                    for (int space = 1; space < i; space++)
+                    {
+                        Console.Write(" ");
+                        count++;
+                    }
+                    for (int j = num - count; j >= 1; j--)
+                    {
+                        Console.Write($"{charac} ");
+                    }
+
+                    Console.WriteLine();
+                }
             }
-            
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input format. Please enter a numeric value.");
+            }
         }
+
         public static void drawSLine(char charac)
         {
-            Console.Write("Enter Lentgh : ");
-            int num = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < num; i++)
+            try
             {
-                System.Console.Write(charac);
+                Console.Write("Enter length: ");
+                int length = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < length; i++)
+                {
+                    Console.Write(charac);
+                }
+                Console.WriteLine();
             }
-            System.Console.WriteLine(Environment.NewLine);
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input format. Please enter a numeric value.");
+            }
         }
     }
 }
